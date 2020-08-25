a15613e788c42f3dadec9226839ddbde8e6d8518
Pedro H. Silva <pedroh_silva@outlook.com>
2020-08-25 01:11:38 -0300

    submitted project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
+const { isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,92 @@ app.use(cors());
 
 const repositories = [];
 
+const getIndexProjectInRepository = (id, repositories) =>
+  repositories.findIndex(({ id: uuid }) => uuid === id)
+
+const isValidIdParam = id => isUuid(id)
+
+const checksIfProjectExists = indexRepository => indexRepository >= 0;
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { url, title, techs } = request.body
+
+  if (!isValidIdParam(id)) return response
+    .status(400)
+    .json({ error: 'invalid parameter, check past data' })
+
+  const projectIndex = getIndexProjectInRepository(id, repositories)
+
+  const isProject = checksIfProjectExists(projectIndex)
+  if (!isProject) return response.status(400).json({ error: 'Project not found' })
+
+  const { likes } = repositories[projectIndex]
+
+  const project = {
+    id,
+    url,
+    title,
+    techs,
+    likes,
+  }
+
+  repositories[projectIndex] = project
+
+  return response.json(project)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const projectIndex = getIndexProjectInRepository(id, repositories)
+
+  if (!isValidIdParam(id)) return response
+    .status(400)
+    .send({ error: 'invalid parameter, check past data' })
+
+  const isProject = checksIfProjectExists(projectIndex)
+  if (!isProject) return response.status(400).send({ error: 'Project not found' })
+
+  repositories.splice(projectIndex, 1)
+
+  return response.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  if (!isValidIdParam(id)) return response
+    .status(400)
+    .json({ error: 'invalid parameter, check past data' })
+
+  const projectIndex = getIndexProjectInRepository(id, repositories)
+
+  const isProject = checksIfProjectExists(projectIndex)
+  if (!isProject) return response.status(400).json({ error: 'Project not found' })
+
+  repositories[projectIndex].likes++
+  const project = repositories[projectIndex]
+  return response.status(201).json(project)
+
 });
 
 module.exports = app;
